9f4c580301772559eaa888a1eb0112e7b7454915
Zion Yang <zionremnant@gmail.com>
2022-03-03 22:13:17 -0800

    added vars js

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ var startBtn = document.getElementById("startBtn");
 var time = 60;
 var timeRemaining = true;
 var timeStart = false;
-var countdown
+var countdown = document.getElementById("countdown");
+home = document.getElementById("home");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
+var choice4 = document.getElementById("choice4");
+var answer = document.getElementById("answer");
+var score = 0;
+var output = "";
+var highScores = [];
+var i = 0;
+// use let?
+
+// questions
+var questionArray = [{
+    question "Q: C"
+}]
